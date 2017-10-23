93bace92704743318443353f663dfb5b429d5fd9
HatsanK <hatsankun@gmail.com>
2017-10-23 20:15:52 +0900

    Init

---
+++ imlk.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @id            imlk
+// @name          GREEM@S KRMenu
+// @version       0.1-userjs
+// @author        l4disk,HatsanK
+// @description   그리마스 메뉴 번역
+// @include       http://imas.gree-apps.net/*
+// @resource      customJS https://raw.githubusercontent.com/HatsanK/imlk/master/custom.js
+// @resource      customCSS https://raw.githubusercontent.com/HatsanK/imlk/master/custom.css
+// @grant         GM_addStyle
+// @grant         GM_getResourceText
+// ==/UserScript==
+
+GM_addStyle (GM_getResourceText ("customCSS") );
