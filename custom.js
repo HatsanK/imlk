56d4175a80488395d1705a8838510cb0b4f71833
HatsanK <hatsankun@gmail.com>
2017-10-27 17:05:08 +0900

    backports 0.6-userjs
    극장 부분의 이미지 번역 추가
    메뉴 부분 번역 추가
    마스터복권 이미지 추가
    
    HatsanK/imlk@c4395670c5 를 기반으로 백포팅 하였습니다.

--- custom.js
+++ custom.js
@@ -7,6 +7,11 @@ $(document).ready(function(){
 	$("[src='http://cdn.bn765.com/b22/20b0715cbf2661726bfd646031239b113b7634b54a6558bdc81366ebb28045d8?c728a3ee2561b78a7e21e7b0e3b08a4a']").attr("src", chrome.extension.getURL("/images/복각.png"));
 	$("[src='http://cdn.bn765.com/aa1/1e4492b2f9669b9684258877f3056bbded1b972ab14e52829270d2895a3b9f37ad4bb12160d43be8b2a06aa05902340dfe1eb1945981ef80a439a2af3ef2fad5']").attr("src", chrome.extension.getURL("/images/라이브복권.png"));
 	$("[src='http://cdn.bn765.com/850/1e4492b2f9669b9684258877f3056bbded1b972ab14e52829270d2895a3b9f376378b97e30f2a7fd52e60ce8364b2d333f45644bbedc7260e0eac5e085df1601']").attr("src", chrome.extension.getURL("/images/이벤트티켓가샤.png"));
+	$("[src='http://cdn.bn765.com/a2c/e4f71985efb653fb760fd54e204afe2513e36a37352693c1bfd329c22f494638?4a6ac627a31c9de73272056afb7dd3fc']").attr("src", chrome.extension.getURL("/images/페이지탑.jpg"));
+	$("[src='http://cdn.bn765.com/ded/4261f0084835ba525df4df83261b0c1de3b630530d2f9e9fd4d3ef6769581af991efc7d0c51ded6189a934da6eedf933?1ad5133c4b5fb4493b8125d3e679c44a']").attr("src", chrome.extension.getURL("/images/추천아이돌.png"));
+	$("[src='http://cdn.bn765.com/042/1e4492b2f9669b9684258877f3056bbded1b972ab14e52829270d2895a3b9f3790547ef888d78c5ed0f82f26ca68b314e398d4ed5f73fd4f1f72e1d9950a336c']").attr("src", chrome.extension.getURL("/images/특별연수.jpg"));
+	$("[src='http://cdn.bn765.com/ad0/4261f0084835ba525df4df83261b0c1df82af2a846ae3ecf897c44d5d700f573434ed7111e9d860e81c2d902d7f7293d?c9c5f45e6e39e4c80b7759212b45301b']").attr("src", chrome.extension.getURL("/images/극장유닛.png"));
+	$("[src='http://cdn.bn765.com/e80/44199bc83e9085aabcfa42d714c72e49553ec2b06301cb72bccf854f3b632d2a?aebe391ac9831bca85fe9798a683e786']").attr("src", chrome.extension.getURL("/images/마스터복권.jpg"));
 
 	/*$(document).ajaxComplete(function(){
 		$("[src='http://cdn.bn765.com/5c2/20b0715cbf2661726bfd646031239b11ea7c7447fba1f1234889047a1f969033?9d0206b8a246389dfdc2748b4992b12b']").attr("src", chrome.extension.getURL("/images/각성.png"));
